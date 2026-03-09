9f8a907fb083207fd14b7f394d567e5ad716cb23
Doug Nelson <doug@peacefulrock.com>
2026-03-09 11:00:26 -0500

    Initial Commit

---
+++ addon-dreamhost-ddns/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/with-contenv bashio
+
+CONFIG_PATH=/data/options.json
+
+API_KEY=$(bashio::config 'api_key')
+DNS_RECORD=$(bashio::config 'dns_record')
+INTERVAL=$(bashio::config 'interval')
+
+echo "Starting DreamHost DDNS updater"
+echo "Record: $DNS_RECORD"
+echo "Interval: $INTERVAL seconds"
+
+while true; do
+    /usr/bin/dreamhost-ddns \
+        --api-key "$API_KEY" \
+        --record "$DNS_RECORD"
+
+    sleep "$INTERVAL"
+done
