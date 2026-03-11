466e63a80f6ba8f6eaf8da431007005515ef7e26
Doug Nelson <doug@peacefulrock.com>
2026-03-11 00:25:39 -0500

    adds log level dropdown

--- addon-dreamhost-ddns/run.sh
+++ addon-dreamhost-ddns/run.sh
@@ -5,13 +5,16 @@ CONFIG_PATH=/data/options.json
 API_KEY=$(bashio::config 'api_key')
 DNS_RECORD=$(bashio::config 'dns_record')
 INTERVAL=$(bashio::config 'interval')
+LOG_LEVEL=$(bashio::config 'log_level')
 
-echo "Starting DreamHost DDNS updater"
-echo "Record: $DNS_RECORD"
-echo "Interval: $INTERVAL seconds"
+bashio::log.info "Starting DreamHost DDNS updater"
+bashio::log.info "Record: $DNS_RECORD"
+bashio::log.info "Interval: $INTERVAL seconds"
+bashio::log.info "Log level: $LOG_LEVEL"
 
 while true; do
-    /usr/bin/dreamhost-ddns --verbose \
+    /usr/bin/dreamhost-ddns \
+        --log-level "$LOG_LEVEL" \
         --api-key "$API_KEY" \
         --record "$DNS_RECORD"
 
