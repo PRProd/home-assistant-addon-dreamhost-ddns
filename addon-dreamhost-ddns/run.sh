33f9cdf52deee840545846b01c3cb70907b3bc79
Doug Nelson <doug@peacefulrock.com>
2026-03-13 14:55:06 -0500

    Adds new binaries and config for IPV6 features

--- addon-dreamhost-ddns/run.sh
+++ addon-dreamhost-ddns/run.sh
@@ -9,6 +9,15 @@ API_KEY=$(bashio::config 'api_key')
 DNS_RECORD=$(bashio::config 'dns_record')
 INTERVAL=$(bashio::config 'interval')
 LOG_LEVEL=$(bashio::config 'log_level')
+IP_MODE=$(bashio::config 'ip_mode')
+
+FLAGS=""
+
+if [ "$IP_MODE" = "ipv4" ]; then
+    FLAGS="--ipv4-only"
+elif [ "$IP_MODE" = "ipv6" ]; then
+    FLAGS="--ipv6-only"
+fi
 
 bashio::log.info "Starting DreamHost DDNS updater"
 bashio::log.info "Record: $DNS_RECORD"
@@ -19,7 +28,8 @@ while true; do
     /usr/bin/dreamhost-ddns \
         --log-level "$LOG_LEVEL" \
         --api-key "$API_KEY" \
-        --record "$DNS_RECORD"
+        --record "$DNS_RECORD" \
+        $FLAGS
 
     sleep "$INTERVAL"
 done
